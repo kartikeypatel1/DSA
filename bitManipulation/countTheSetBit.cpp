c3db070e7cf1a659dda9c54bf9a66398728a03a9
kartikey <kartikey40patel@gmail.com>
2026-08-05 02:15:50 +0530

    solve problems

---
+++ bitManipulation/countTheSetBit.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countSetBit(int n){
+    int count=0;
+    while(n>0){
+        n=n&(n-1);
+        count++;
+    }
+    return count;
+}
+int main()  {
+    int n;
+    cin>>n;
+    cout<<countSetBit(n);
+    return 0;
+}
