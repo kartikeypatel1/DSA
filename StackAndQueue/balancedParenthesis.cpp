0a8e904ecfe5f3d5d765cfe2f70f16059e671b38
kartikey <kartikey40patel@gmail.com>
2026-08-23 01:31:58 +0530

    stack and queue problems

---
+++ StackAndQueue/balancedParenthesis.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool balanced(string &s){
+     stack<char> st;
+        
+        for(char ch : s) {
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+            else {
+                if(st.empty()) return false;
+                char top = st.top();                
+                if((ch == ')' && top == '(') ||
+                   (ch == '}' && top == '{') ||
+                   (ch == ']' && top == '[')) {
+                    st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }       
+        return st.empty();
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<balanced(s);
+}
